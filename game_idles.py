cf443a002fc46bba6fbfdc94708814b156072440
Akkiraj1234 <141214556+Akkiraj1234@users.noreply.github.com>
2024-04-28 19:14:51 +0530

    added file:   game_idles.py 	added file:   game_screens.py 	added file:   main.py 	added file:   player_info/demo_player.info 	added file:   settings.json

---
+++ game_idles.py
@@ -0,0 +1,310 @@
+from tkinter import Canvas
+from random import randint,choice
+class Snake:
+    """Represents a snake in the game.
+
+    Attributes:
+        canvas (Canvas): The canvas on which the snake will be drawn.
+        length (int): The initial length of the snake.
+        coordinates (tuple): The initial coordinates of the snake.
+        color (str): The color of the snake.
+        box_size (int): The size of each box representing a segment of the snake.
+
+    Methods:
+        __init__(canvas, length, coordinates, color, box_size): Initialize a new Snake object.
+        _create_snake(): Create the snake on the canvas.
+        move_snake(x, y, remove): Move the snake to the new coordinates.
+        _create_body(x, y): Create a segment of the snake.
+    """
+    
+    def __init__(self,canvas:Canvas,lenght:int,coordinates:tuple,color:str,box_size:int) -> None :
+        """Initialize the Snake object on the canvas.
+
+        Args:
+            canvas (Canvas): The canvas on which the snake will be drawn.
+            length (int): The initial length of the snake.
+            coordinates (tuple): The initial coordinates of the snake.
+            color (str): The color of the snake.
+            box_size (int): The size of each box representing a segment of the snake.
+        """
+        self.box_size = box_size
+        self.color = color
+        self.canvas = canvas
+        self.lenght=lenght
+        self.coordinates = coordinates
+        self._create_snake()
+        
+    def _create_snake(self)-> None:
+        """Create the snake on the canvas."""
+        self.snake_coordinates=[self.coordinates]*self.lenght
+        self.snake_body=[self._create_body(x,y) for x,y in self.snake_coordinates]
+        
+    def move_snake(self,x:int,y:int,remove:bool)-> None:
+        """Move the snake to the new coordinates.
+
+        Args:
+            x (int): The x-coordinate of the new position.
+            y (int): The y-coordinate of the new position.
+            remove (bool): Whether to remove the last segment of the snake.
+        """
+        self.snake_coordinates.insert(0,(x,y))
+        self.snake_body.insert(0,self._create_body(x,y))
+        
+        if remove:
+            self.snake_coordinates.pop()
+            self.canvas.delete(self.snake_body.pop())
+    
+    def _create_body(self,x:int,y:int)-> None:
+        """Create a segment of the snake.
+
+        Args:
+            x (int): The x-coordinate of the segment.
+            y (int): The y-coordinate of the segment.
+
+        Returns:
+            int: The id of the created segment on the canvas.
+        """
+        square= self.canvas.create_rectangle(x,y,x+self.box_size,y+self.box_size,fill=self.color)
+        return square
+    
+    
+class Food:
+    """Represents a food item in the game.
+
+    Attributes:
+        canvas (Canvas): The canvas on which the food item will be drawn.
+        box_size (int): The size of each box representing the food item.
+        color (str): The color of the food item.
+        game_width (int): The width of the game area.
+        game_height (int): The height of the game area.
+        food (int): The ID of the current food item on the canvas.
+        x (int): The x-coordinate of the food item.
+        y (int): The y-coordinate of the food item.
+
+    Methods:
+        __init__(canvas, box_size, color, game_width, game_height, food_type="oval"): Initialize a new Food object.
+        _create_food_oval(): Create an oval-shaped food item on the canvas.
+        _create_food_square(): Create a square-shaped food item on the canvas.
+        new_coordinates(): Generate new random coordinates for the food item.
+        _delete_food(): Delete the current food item from the canvas.
+        new_food(food_type): Create a new food item of the specified type.
+    """
+    def __init__(self, canvas:Canvas, box_size:int, color:str ,game_width:int ,game_height:int, food_type:str="oval")-> None:
+        """Initialize the Food object.
+
+        Args:
+            canvas (Canvas): The canvas on which the food item will be drawn.
+            box_size (int): The size of each box representing the food item.
+            color (str): The color of the food item.
+            game_width (int): The width of the game area.
+            game_height (int): The height of the game area.
+            food_type (str, optional): The type of the food item ("oval" or "square"). Defaults to "oval".
+        """
+        self.canvas = canvas
+        self.box_size = box_size
+        self.color = color
+        self.game_width = game_width
+        self.game_height = game_height
+        self.food=None
+        self.x=0
+        self.y=0
+        self.new_coordinates()
+        self.new_food(food_type)
+        
+    def _create_food_oval(self):
+        """Create an oval-shaped food item."""
+        self.food=self.canvas.create_oval(self.x,self.y,self.x+self.box_size,self.y+self.box_size,fill=self.color)
+    
+    def _create_food_square(self):
+        """Create a square-shaped food item."""
+        # self.food=self.canvas.create_polygon(self.x,self.y,self.x+self.box_size,self.y+self.box_size,fill=self.color)
+        self.food = self.canvas.create_polygon(self.x, self.y, self.x + self.box_size, self.y + self.box_size, fill=self.color)
+        
+    def new_coordinates(self):
+        """Generate new random coordinates for the food item."""
+        self.x = randint(0, (self.game_width // self.box_size)-1)* self.box_size
+        self.y =  randint(0, (self.game_height // self.box_size)-1)* self.box_size
+    
+    def _delete_food(self):
+        """Delete the current food item from the canvas."""
+        self.canvas.delete(self.food)
+    
+    def new_food(self,food_type:str):
+        """Create a new food item of the specified type.
+
+        Args:
+            food_type (str): The type of the food item ("oval" or "square" or "any" ).
+        """
+        if self.food:
+            self._delete_food()
+        self.new_coordinates()
+        
+        if food_type == "oval":
+            self._create_food_oval()
+        elif food_type == "square":
+            self._create_food_square()
+        else:
+            self._create_food_oval()
+            
+            
+class Heart:
+    """
+    Represents a heart shape drawn on a canvas.
+
+    This class provides methods to add, remove, and manipulate heart shapes on a canvas.
+
+    Attributes:
+        canvas (Canvas): The canvas on which the hearts are drawn.
+        coordinates (tuple): The initial coordinates (x, y) of the top-left corner of the heart.
+        box_size (int): The size of the bounding box of the heart.
+        color (str): The color of the heart.
+        distance (int): The distance between each heart when added.
+        hearts_list (list): A list to store the IDs of the heart shapes drawn on the canvas.
+        limit (int or None): The maximum number of hearts allowed on the canvas. If set, adding more hearts will be limited.
+    """
+    def __init__(self,canvas:Canvas , cordnites:tuple , box_size:int , color:str , inisial_heart=1):
+        """
+        Initialize a Heart object.
+
+        Args:
+            canvas (Canvas): The canvas on which the heart will be drawn.
+            coordinates (tuple): The initial coordinates (x, y) of the top-left corner of the heart.
+            box_size (int): The size of the bounding box of the heart.
+            color (str): The color of the heart.
+            initial_heart (int, optional): The number of hearts to initialize. Defaults to 1.
+        """
+        self.canvas = canvas
+        self.cordinates = cordnites
+        self.box_size = box_size
+        self.color = color
+        self.distance = self.box_size // 4
+        self.hearts_list = []
+        self.limit = None
+        self.heart()
+        for _ in range(inisial_heart-1):
+            self.add_one_heart()
+    
+    def add_one_heart(self):
+        """
+        Add one heart to the canvas.
+
+        If the limit is set and the number of hearts exceeds the limit, no heart will be added.
+
+        Returns:
+            None
+        """
+        if self.limit and len(self.hearts_list)>= self.limit:
+            return None
+        self.cordinates = (self.cordinates[0]-self.box_size-self.distance , self.cordinates[1])
+        self.heart()
+        
+    def remmove_heart(self):
+        """
+        Remove the last heart from the canvas.
+
+        Returns:
+            None
+        """
+        if self.hearts_list == []:
+            return None
+        for ids in self.hearts_list.pop():
+            self.canvas.delete(ids)
+        self.cordinates = (self.cordinates[0]+self.box_size+self.distance , self.cordinates[1])
+        
+    def heart(self):
+        """
+        Draw a heart shape on the canvas.
+
+        This method calculates the coordinates and dimensions of the heart shape
+        based on the given parameters and draws it on the canvas.
+
+        Returns:
+            None
+        """
+        x1 , y1 = self.cordinates
+        x2 , y2 = x1 + self.box_size , y1 + self.box_size
+
+        new_y1 = (self.box_size / 2) + y1
+        new_x1 = (self.box_size / 2) + x1
+        radious = (new_y1 - y1) / 2
+        
+        first = self.canvas.create_arc(x1,new_y1-radious,new_x1,new_y1+radious,fill=self.color,start=0,extent=180)
+        secoend = self.canvas.create_arc(new_x1,new_y1-radious,x2,new_y1+radious,fill=self.color,start=0,extent=180)
+        third = self.canvas.create_polygon(x1,new_y1,x2,new_y1,(x1+x2)/2,y2,fill=self.color)
+        self.hearts_list.append((first,secoend,third))
+        
+
+class goofy_Snakes:
+    """Represents a goofy snake in the game.
+
+    Attributes:
+        master (Canvas): The canvas on which the snake will be drawn.
+        color (str): The color of the snake.
+        coordinates (tuple): The initial coordinates of the snake.
+        length (int): The initial length of the snake.
+        initial_direction (str): The initial direction of the snake.
+        box_size (int): The size of each box representing a segment of the snake.
+        snake (Snake): The Snake object representing the snake on the canvas.
+        coordinate (tuple): The current coordinates of the snake's head.
+
+    Methods:
+        __init__(master, color, coordinates, length, initial_direction, box_size): Initialize a new goofy_Snakes object.
+        random_direction(): Randomly choose a new direction for the snake.
+        calculate_new_coordinates(): Calculate new coordinates for the snake's head based on its current direction.
+        move_the_snakes(): Move the snake to the new coordinates.
+    """
+    
+    def __init__(self,master:Canvas, color:str ,coordinates:tuple, lenght:int ,inisial_direction:str, box_size:int)-> None:
+        """Initialize a new goofy_Snakes object.
+
+        Args:
+            master (Canvas): The canvas on which the snake will be drawn.
+            color (str): The color of the snake.
+            coordinates (tuple): The initial coordinates of the snake.
+            length (int): The initial length of the snake.
+            initial_direction (str): The initial direction of the snake.
+            box_size (int): The size of each box representing a segment of the snake.
+        """
+        self.master = master
+        self.color = color
+        self.coordinates = coordinates
+        self.lenght = lenght
+        self.direction = inisial_direction
+        self.box_size = box_size
+        self.snake = Snake(self.master, self.lenght, self.coordinates, self.color, self.box_size)
+        
+    def random_direction(self):
+        """Randomly choose a new direction for the snake."""
+        new_direction = choice(("down","right","down","right","down","left","up","down"))
+        if new_direction in ("down","up") and self.direction in ("down","up"):
+            pass
+        elif new_direction in ("right","left") and self.direction in ("right","left"):
+            pass
+        else: self.direction = new_direction
+    
+    def calculate_new_coordinates(self):
+        """Calculate new coordinates for the snake's head based on its current direction."""
+        self.random_direction()
+        x , y =self.snake.snake_coordinates[0]
+        if self.direction == "up":
+            y -= self.box_size
+        elif self.direction == "down":
+            y += self.box_size
+        elif self.direction == "right":
+            x += self.box_size
+        elif self.direction == "left":
+            x -= self.box_size
+        
+        #adjusting screen :0
+        canvas_width = self.master.winfo_width()
+        canvas_height = self.master.winfo_height()
+        x = canvas_width if x < 0 else 0 if x > canvas_width else x
+        y = canvas_height if y < 0 else 0 if y > canvas_height else y
+        
+        self.coordinates = (x,y)
+    
+    def move_the_snakes(self):
+        """Move the snake to the new coordinates."""
+        self.calculate_new_coordinates()
+        x , y = self.coordinates
+        self.snake.move_snake(x,y,True)
---
+++ game_screens.py
@@ -0,0 +1,130 @@
+from tkinter import Canvas
+from random import choice
+from game_idles import Food,goofy_Snakes
+
+
+class inisial_screens:
+    """A class representing an initial screen setup for a game.
+
+    This class provides methods for setting up the initial screen of a game, including adding snakes, food items, starting animations, and managing animations.
+
+    Attributes:
+        master: The master widget where the initial screen will be placed.
+        background_color (str): The background color of the game screen.
+        game_width (int): The width of the game screen.
+        box_size (int): The size of each box or unit in the game grid.
+        game_height (int): The height of the game screen.
+        speed (int): The speed of animations in milliseconds.
+        child_window (tkinter.Canvas): The canvas widget representing the game screen.
+        food (Food): The food item on the game screen.
+        snakes (list): A list containing snake objects on the game screen.
+        animation_after_ids: A reference to the animation after method IDs for managing animation loops.
+
+    Methods:
+        __init__: Initializes the initial screen with provided parameters.
+        add_snakes: Adds a snake to the game screen.
+        add_food: Adds a food item to the game screen.
+        start_animation: Starts the animation loop for moving snakes and updating the game state.
+        stop_animation: Stops the animation loop.
+        add_button: Adds a button widget to the game screen.
+
+    Example:
+        # Initialize the initial screen
+        initial_screen = inisial_screens(master, background_color='black', game_width=800, box_size=20, game_height=600, speed=100)
+
+        # Add a snake to the initial screen
+        initial_screen.add_snakes(color='green', coordinates=(5, 5), length=3, direction='right')
+
+        # Add food to the initial screen
+        initial_screen.add_food(color='red', food_type='oval', random_color=True)
+
+        # Start the animation loop
+        initial_screen.start_animation()
+
+        # Stop the animation loop
+        initial_screen.stop_animation()
+
+        # Add a button to the game screen
+        initial_screen.add_button(button1, button2, button3)
+    """
+    
+    def __init__(self,master,background_color:str,game_width:int,box_size:int, game_height:int,speed:int,pack=True) -> None:
+        """Initialize the initial screen of the game.
+
+        Args:
+            master: The master widget where the initial screen will be placed.
+            background_color (str): The background color of the game screen.
+            game_width (int): The width of the game screen.
+            box_size (int): The size of each box or unit in the game grid.
+            game_height (int): The height of the game screen.
+            speed (int): The speed of animations in milliseconds.
+            pack (bool): Whether to automatically pack the canvas widget (default is True).
+        """
+        self.master = master
+        self.background_color = background_color
+        self.game_width = game_width
+        self.game_height = game_height
+        self.box_size = box_size
+        self.speed=speed
+        
+        self.child_window = Canvas(self.master,bg=self.background_color,height=self.game_height,width=self.game_width)
+        if pack: self.child_window.pack()
+        self.food = None
+        self.snakes = []
+        self.animation_after_ids=None
+    
+    def add_to_window(self):
+        self.child_window.pack()
+    
+    def add_snakes(self, color:str ,coordinates:tuple, lenght:int, direction:str)-> None:
+        """Add a snake to the game screen.
+
+        Args:
+            color (str): The color of the snake.
+            coordinates (tuple): The initial coordinates of the snake.
+            length (int): The length of the snake.
+            direction (str): The initial direction of the snake ('up', 'down', 'left', 'right').
+        """
+        coordinates = [i * self.box_size for i in coordinates]
+        self.snakes.append(
+            goofy_Snakes(self.child_window,color,coordinates,lenght,direction,self.box_size)
+        )
+    
+    def add_food(self, color:str, food_type="oval",randome_color=False):
+        """Add a food item to the game screen.
+
+        Args:
+            color (str): The color of the food item.
+            food_type (str): The type of food item ('oval' or 'rectangle', default is 'oval').
+            random_color (bool): Whether to randomize the color of the food item (default is False).
+        """
+        self.food_type=food_type
+        self.random_color=randome_color
+        self.food=Food(self.child_window, self.box_size, color, self.game_width, self.game_height, food_type)
+        
+    def start_animation(self):
+        """Start the animation loop for moving snakes and updating the game state."""
+        for snake in self.snakes:
+            snake.move_the_snakes()
+            x,y= snake.coordinates
+            foodx,foody=self.food.x,self.food.y
+            if x == foodx and y == foody:
+                if self.random_color:
+                    self.food.color = choice(("blue","yellow","red","pink"))
+                self.food.new_food(self.food_type)
+        self.animation_after_ids=self.child_window.after(self.speed,self.start_animation)
+    
+    def stop_animation(self):
+        """Stop the animation loop."""
+        self.child_window.after_cancel(self.animation_after_ids)
+    
+    def add_button(self,*args):
+        """Add button widgets to the game screen.
+
+        Args:
+            *buttons: Button widgets to be added to the game screen.
+        """
+        num = self.box_size-(self.box_size*2)
+        for button in args:
+            self.child_window.create_window(self.game_width//2,self.game_height//2+num,window=button)
+            num += self.box_size
